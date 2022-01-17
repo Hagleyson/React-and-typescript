0896d6b91fc19b6c4c340208ddd97165469f5272
hagleyson <hagleyson@gmail.com>
2022-01-17 00:03:56 -0300

    Working with refs & useRef

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import NewTodo from "./components/NewTodo";
 import Todos from "./components/Todos";
 import Todo from "./models/todo";
 function App() {
   const todos = [new Todo("Learn React"), new Todo("Lean TypeScript")];
   return (
     <div>
+      <NewTodo />
       <Todos items={todos}>
         <h1>hagleyson</h1>
       </Todos>
